7e6ddb3aec7794933ca3b5e1679f2d5c2da92b53
Jayasuriya P <146417429+Jayasuriya49@users.noreply.github.com>
2026-01-11 16:42:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function getTodoListFromLocalStorage() {
 let todoList = getTodoListFromLocalStorage();
 
 let todosCount = todoList.length;
-saveTodoButton.onclick = function() {
+saveTodoButton?.onclick = function() {
     localStorage.setItem("todoList", JSON.stringify(todoList));
 };
 
